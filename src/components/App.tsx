afc527356c3cdf58314608f64e6cd20a8a1d572d
nathanhall762 <nathanhall762@gmail.com>
2023-08-14 13:42:56 -0500

    musician card and page components added

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,20 @@
 import { useState, useEffect } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
-import reactLogo from '../assets/react.svg';
-import viteLogo from '/vite.svg';
 import '../css/App.css';
 import { db } from '../firebase';
+import MusicianPage, { Musician } from './MusicianPage';
+import 'font-awesome/css/font-awesome.min.css';
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [bandName, setBandName] = useState('');
+  const [musicians, setMusicians] = useState<Musician[]>([]);
 
   const getData = async () => {
     const querySnapshot = await getDocs(collection(db, 'musicians'));
     // console.log(querySnapshot.docs);
-    querySnapshot.docs.map((doc) => {
-      console.log(doc.data().name)
-      const name = doc.data().name;
-      setBandName(name);
-    });
+    const fetchedMusicians: Musician[] = querySnapshot.docs.map((doc) => doc.data() as Musician);
+    setMusicians(fetchedMusicians);
   };
 
   useEffect(() => {
@@ -25,29 +23,13 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>
-        {bandName ? bandName : 'Loading'}
-      </p>
+      {musicians.length ? (
+        musicians.map((musician, index) => (
+          <MusicianPage key={index} musician={musician} />
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   );
 }
