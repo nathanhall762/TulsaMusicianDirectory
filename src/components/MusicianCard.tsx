6ce891bcbb2b86feb09b5cc720e323453823c30b
nathanhall762 <nathanhall762@gmail.com>
2023-08-17 15:35:22 -0500

    fixed styling and added card animation

--- src/components/MusicianCard.tsx
+++ src/components/MusicianCard.tsx
@@ -61,68 +61,72 @@ const MusicianCard: React.FC<MusicianCardProps> = ({
 
   return (
     <div className={styles.musicianCard} onClick={clickCard}>
-      <h2>{name}</h2>
-      <img className={styles.profileImage} src={profileImage} alt={name} />
-      <p>Genre: {genre.length !== 0 ? genre.join(', ') : 'NA'}</p>
-      <div className={styles.socialLinks}>
-        {bandcamp && (
-          <a
-            href={typeof bandcampURL == 'string' ? bandcampURL : ''}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <i className='fa fa-bandcamp' aria-hidden='true'></i>
-          </a>
-        )}
-        {spotify && (
-          <a href={spotify} target='_blank' rel='noopener noreferrer'>
-            <i className='fa fa-spotify' aria-hidden='true'></i>
-          </a>
-        )}
-        {youtube && (
-          <a href={youtube} target='_blank' rel='noopener noreferrer'>
-            <i className='fa fa-youtube' aria-hidden='true'></i>
-          </a>
-        )}
-        {soundcloud && (
-          <a
-            href={
-              typeof soundcloudProfileURL == 'string'
-                ? soundcloudProfileURL
-                : ''
-            }
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <i className='fa fa-soundcloud' aria-hidden='true'></i>
-          </a>
-        )}
-        {facebook && (
-          <a href={facebook} target='_blank' rel='noopener noreferrer'>
-            <i className='fa fa-facebook' aria-hidden='true'></i>
-          </a>
-        )}
-        {instagram && (
-          <a href={instagram} target='_blank' rel='noopener noreferrer'>
-            <i className='fa fa-instagram' aria-hidden='true'></i>
-          </a>
-        )}
-        {tiktok && (
-          <a href={tiktok} target='_blank' rel='noopener noreferrer'>
-            <i className='fa fa-tiktok' aria-hidden='true'></i>
-          </a>
-        )}
-        {threads && (
-          <a href={threads} target='_blank' rel='noopener noreferrer'>
-            {/* Placeholder icon for Threads (since I'm not sure if there's a FontAwesome icon for it yet) */}
-            <i className='fa fa-threads' aria-hidden='true'></i>
-          </a>
-        )}
-        {twitch && (
-          <a href={twitch} target='_blank' rel='noopener noreferrer'>
-            <i className='fa fa-twitch' aria-hidden='true'></i>
-          </a>
-        )}
+      <div className={styles.name}>
+        <h2>{name}</h2>
+      </div>
+      <div className={styles.keyInfo}>
+        <img className={styles.profileImage} src={profileImage} alt={name} />
+        <p>Genre: {genre.length !== 0 ? genre.join(', ') : 'NA'}</p>
+        <div className={styles.socialLinks}>
+          {bandcamp && (
+            <a
+              href={typeof bandcampURL == 'string' ? bandcampURL : ''}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='fa fa-bandcamp' aria-hidden='true'></i>
+            </a>
+          )}
+          {spotify && (
+            <a href={spotify} target='_blank' rel='noopener noreferrer'>
+              <i className='fa fa-spotify' aria-hidden='true'></i>
+            </a>
+          )}
+          {youtube && (
+            <a href={youtube} target='_blank' rel='noopener noreferrer'>
+              <i className='fa fa-youtube' aria-hidden='true'></i>
+            </a>
+          )}
+          {soundcloud && (
+            <a
+              href={
+                typeof soundcloudProfileURL == 'string'
+                  ? soundcloudProfileURL
+                  : ''
+              }
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='fa fa-soundcloud' aria-hidden='true'></i>
+            </a>
+          )}
+          {facebook && (
+            <a href={facebook} target='_blank' rel='noopener noreferrer'>
+              <i className='fa fa-facebook' aria-hidden='true'></i>
+            </a>
+          )}
+          {instagram && (
+            <a href={instagram} target='_blank' rel='noopener noreferrer'>
+              <i className='fa fa-instagram' aria-hidden='true'></i>
+            </a>
+          )}
+          {tiktok && (
+            <a href={tiktok} target='_blank' rel='noopener noreferrer'>
+              <i className='fa fa-tiktok' aria-hidden='true'></i>
+            </a>
+          )}
+          {threads && (
+            <a href={threads} target='_blank' rel='noopener noreferrer'>
+              {/* Placeholder icon for Threads (since I'm not sure if there's a FontAwesome icon for it yet) */}
+              <i className='fa fa-threads' aria-hidden='true'></i>
+            </a>
+          )}
+          {twitch && (
+            <a href={twitch} target='_blank' rel='noopener noreferrer'>
+              <i className='fa fa-twitch' aria-hidden='true'></i>
+            </a>
+          )}
+        </div>
       </div>
     </div>
   );
