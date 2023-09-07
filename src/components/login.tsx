205b1a49267c9591a835524969761d6bfefb11f0
nathanhall762 <nathanhall762@gmail.com>
2023-09-07 11:15:00 -0500

    changed header and added analytics

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -7,6 +7,7 @@ import { auth } from '../firebase';
 import { FirebaseError } from 'firebase/app';
 import { useOutletContext } from 'react-router-dom';
 import { OutletContextProps } from '../types';
+import styles from '../css/Login.module.css';
 
 interface LoginData {
   email: string;
@@ -75,25 +76,38 @@ const Login = () => {
     return <p>Hello {user.user.email}</p>;
   } else {
     return (
-      <div>
-        <input
-          type='email'
-          name='email'
-          placeholder='email'
-          value={loginData.email}
-          onChange={handleInputChange}
-        />
-        <input
-          type='password'
-          name='password'
-          placeholder='password'
-          minLength={6}
-          value={loginData.password}
-          onChange={handleInputChange}
-        />
-        <div>
-          <button onClick={signUp}>sign up</button>
-          <button onClick={signIn}>login</button>
+      <div className={styles.loginContainer}>
+        <h1>The Tulsa Musician Directory</h1>
+        <ul>
+          How to support:
+          <li>Discover local bands by exploring the directory and listening to music previews.</li>
+          <li>Use the social links to follow and engage with artist content.</li>
+          <li>Use the music links to stream the artists' music.</li>
+          <li>Help build a working directory of Tulsa musicians by signing
+          up to add bands/musicians!</li>
+        </ul>
+        <div className={styles.loginForm}>
+          <div className={styles.inputContainer}>
+            <input
+              type='email'
+              name='email'
+              placeholder='email'
+              value={loginData.email}
+              onChange={handleInputChange}
+            />
+            <input
+              type='password'
+              name='password'
+              placeholder='password'
+              minLength={6}
+              value={loginData.password}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className={styles.buttonBox}>
+            <button onClick={signUp}>sign up</button>
+            <button onClick={signIn}>login</button>
+          </div>
         </div>
       </div>
     );
