47be60e6b815e7af85961423bc9bee52c599fb89
EthanWakeford <ethanwakeford@gmail.com>
2023-09-29 12:23:33 -0500

    added spotify data scripts

--- SpotifyToFirestore/audioFeaturesToFirestore.js
+++ SpotifyToFirestore/audioFeaturesToFirestore.js
@@ -2,6 +2,7 @@
 import { adminDb } from './firebaseAdminConfig.js';
 import TrackFeatures from './getSpotifyAudioFeatures.js';
 
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function getSpotifyAudioFeatures() {
   const t = new TrackFeatures({
@@ -10,58 +11,140 @@ async function getSpotifyAudioFeatures() {
   });
   await t.createToken();
 
-  // TODO: get all artist ids from musicians collection in firestore
-  // currently, we're just using one artist id
-  const artistsId = ['5TjSLDEdLdGrrG4I7yyAVl', '5TjSLDEdLdGrrG4I7yyAVl'];
-  const features = await t.getAllTrackFeatures(artistsId);
-  // const features = await t.getFeaturesFromArtist(artistsId);
+  // const artistsId = ['5TjSLDEdLdGrrG4I7yyAVl', '5TjSLDEdLdGrrG4I7yyAVl'];
+  const artistIds = await getAllArtistsFromFirestore();
+  // const features = await t.getAllTrackFeatures(artistIds);
 
+  const features = [];
 
+  for (const artist of artistIds) {
+    const artistFeatures = await t.getFeaturesFromArtist(artist);
+    await sleep(1000);
+    features.push(artistFeatures);
+  }
 
-  // processAudioFeatures(features);
+  console.log('features', features);
 
-  features.map(processAudioFeatures)
+  features.map(processAudioFeatures);
 
-  // const stuff = await t.getFeaturesFromArtist('5TjSLDEdLdGrrG4I7yyAVl');
-  // console.log('the features:', stuff);
+  console.log('it all worked i guess');
 }
 
-function processAudioFeatures(audioFeatures) {
-  // console.log(typeof(audioFeatures)); // object
-  // console.log(audioFeatures); // expected return of audiofeatures
-  
-  // Flatten the data structure
-  const flattenedData = audioFeatures.flatMap(item => item.audio_features);
-  
-  // Map the Data
-  const mappedData = flattenedData.map((track) => {
-    console.log(track)
-    return {
-      id: track.id,
-      acousticness: track.acousticness,
-      danceability: track.danceability,
-      energy: track.energy,
-      instrumentalness: track.instrumentalness,
-      liveness: track.liveness,
-      loudness: track.loudness,
-      valence: track.valence,
-    };
-  });
+async function getAllArtistsFromFirestore() {
+  const fetchedMusicians = await adminDb
+    .collection('musicians')
+    .get()
+    .then((res) => res.docs.map((doc) => doc.data()));
+
+  const spotifyLinks = fetchedMusicians.map(
+    (musician) => musician.music.spotify
+  );
+
+  const re = /\/artist\/(\w+)/;
+  const artistIds = spotifyLinks
+    .filter((link) => link !== '')
+    .map((link) => link.match(re)[1]);
+
+  return artistIds;
+}
+
+async function processAudioFeatures(artistAudioFeatures) {
+  const collectionName = 'audioFeatures';
+
+  console.log('artist features 1', artistAudioFeatures);
+  console.log(
+    'trackfeatures',
+    artistAudioFeatures.albumTracksFeatures[0].trackFeatures
+  );
+
+  // // Flatten the data structure
+  // const flattenedData =
+  //   artistAudioFeatures.albumTracksFeatures.trackFeatures.flatMap(
+  //     (item) => item.audio_features
+  //   );
+
+  // // Map the Data
+  // const mappedData = flattenedData.map((track) => {
+  //   return {
+  //     id: track.id,
+  //     acousticness: track.acousticness,
+  //     danceability: track.danceability,
+  //     energy: track.energy,
+  //     instrumentalness: track.instrumentalness,
+  //     liveness: track.liveness,
+  //     loudness: track.loudness,
+  //     valence: track.valence,
+  //   };
+  // });
+
+  // artistAudioFeatures.albumTracksFeatures.trackFeatures = mappedData;
+
+  console.log('artist features', artistAudioFeatures);
 
   // Push Data to Firebase
-  const collection = adminDb.collection('audioFeatures');
-  mappedData.forEach(async (track) => {
+  const collection = adminDb.collection(collectionName);
+  try {
+    if (!artistAudioFeatures.artistId) {
+      console.error(
+        `artist ID is undefined for artist: ${artistAudioFeatures}`
+      );
+      return;
+    }
+    await collection.doc(artistAudioFeatures.artistId).set(artistAudioFeatures);
+    console.log(`Added track with ID: ${artistAudioFeatures.artistId}`);
+  } catch (error) {
+    console.error(
+      `Error adding track with ID: ${artistAudioFeatures.artistId}.${error}`
+    );
+  }
+}
+
+async function testPush() {
+  const newCollection = 'testCollection';
+
+  const artistId = 'fakeId';
+  const otherArtist = 'otherFakeId';
+  const albumId = 'fakeAlbum';
+  const features = ['fakeFeatures', 'features 2'];
+
+  const mappedData = [
+    {
+      artistId: artistId,
+      albumTracksFeatures: {
+        albumId: albumId,
+        TrackFeatures: {
+          features,
+        },
+      },
+    },
+    {
+      artistId: otherArtist,
+      albumTracksFeatures: {
+        albumId: albumId,
+        TrackFeatures: {
+          features,
+        },
+      },
+    },
+  ];
+
+  const collection = adminDb.collection(newCollection);
+  mappedData.forEach(async (artist) => {
     try {
-      if (!track.id) {
-        console.error(`Track ID is undefined for track: ${JSON.stringify(track)}`);
+      if (!artist.artistId) {
+        console.error(
+          `artist ID is undefined for artist: ${JSON.stringify(artist)}`
+        );
         return;
       }
-      await collection.doc(track.id).set(track);
-      console.log(`Added track with ID: ${track.id}`);
+      await collection.doc(artist.artistId).set(artist);
+      console.log(`Added track with ID: ${artist.artistId}`);
     } catch (error) {
-      console.error(`Error adding track with ID: ${track.id}. Error: ${error}`);
+      console.error(`Error adding track with ID: ${artist.artistId}.${error}`);
     }
   });
 }
+
+// testPush();
 
 getSpotifyAudioFeatures();
