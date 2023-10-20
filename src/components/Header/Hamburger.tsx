78a69feb37564912d3f1684445e1b671c6d7c0f6
EthanWakeford <ethanwakeford@gmail.com>
2023-10-20 13:14:44 -0500

    adding hamburger navigation

--- src/components/Header/Hamburger.tsx
+++ src/components/Header/Hamburger.tsx
@@ -1,15 +1,42 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export default () => {
   return (
     <NavList>
-      <li>About</li>
-      <li>Directory</li>
-      <li>Discover</li>
+      <li>
+        <StyledLink to='/about'>About</StyledLink>
+      </li>
+      <li>
+        <StyledLink to='/'>Directory</StyledLink>
+      </li>
+      <li>
+        <StyledLink to='/discover'>Discover</StyledLink>
+      </li>
     </NavList>
   );
 };
 
 const NavList = styled.ul`
   background-color: var(--color-background-alt);
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: var(--color-background-main) solid;
+  box-shadow: 100px;
+  :hover {
+    color: red;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: var(--color-text-inverse);
 `;
