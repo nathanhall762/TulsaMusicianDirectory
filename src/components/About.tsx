55f3d59d1f1307b5fdf97234d579e9f257dcda92
Nathan Hall <nathanhall762@gmail.com>
2023-10-26 14:35:08 -0500

    added about page copy

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,87 @@ const About = () => {
   return (
     <>
       <h2>About Us</h2>
-      <p>We Are The Tulsa Musician Directory</p>
+      <p>
+        Tulsa Musician Directory is your gateway to Tulsa's thriving music
+        scene. Our web application provides a straightforward way to discover
+        local musicians and their talent. We rely on contributions from the
+        community to ensure the accuracy of our directory, making it a reliable
+        tool for local music discovery. Our commitment is to celebrate and
+        support Tulsa's music community by offering a resource where fans can
+        easily connect with their favorite local talent. Join us in exploring
+        the richness of Tulsa's music culture and become a part of our growing
+        community.
+      </p>
+      <p>
+        Support for local music is a powerful force for positive change in our
+        community. When you choose local musicians, you're not just purchasing
+        music; you're contributing to a cycle of creativity and commerce. Your
+        investments in their work enable artists to reinvest in their craft,
+        fueling a thriving cultural scene that attracts and supports even more
+        talent. It's a ripple effect that strengthens our local economy while
+        nurturing our shared culture. So why should it be such a CHORE to
+        discover local music you’ll love? We think the problem is the digital
+        landscape of music discovery. Namely, the platforms that provide the
+        most reach are designed explicitly to sell ads and data, not to educate-
+        to provide information to music lovers about the culture right outside
+        their front door and bridge the gap between the local artists and their
+        city.
+      </p>
+      <p>
+        But what if discovering local music you'll love didn't have to be a
+        chore? In 2023, it's high time we had a simple solution—a directory of
+        musicians in Tulsa. The Tulsa Musician Directory is here to fill that
+        gap. We're the platform that makes it easy to find and connect with the
+        incredible musicians right in our city. Plain and simple, we're the
+        missing link that brings our community and its artists together.
+      </p>
+      <p>
+        At the heart of the Tulsa Musician Directory lies the spirit of
+        collaboration and community. We firmly believe that the power of our
+        platform is driven by the collective effort of our users, musicians, and
+        music enthusiasts alike. Together, we are building something
+        extraordinary—an inclusive, comprehensive directory of Tulsa's musical
+        talent. Our platform thrives on your contributions. Whether you're a
+        musician looking to showcase your work or a music lover with a passion
+        for discovery, you play a pivotal role in supporting our directory. With
+        your help, we can ensure that our directory accurately represents the
+        wealth of musical talent in our city.
+      </p>
+      <p>
+        Our long-term vision for the Tulsa Musician Directory is to create a
+        versatile tool that can be easily replicated in any city or region. With
+        just a few dedicated admins and basic coding knowledge, we aim to
+        empower communities to build their own musician directories. This
+        scalability allows us to expand our platform's reach, connecting
+        musicians and enthusiasts across the nation and beyond. Together, we can
+        foster thriving local music scenes and celebrate the unique cultures of
+        different cities and regions.
+      </p>
+      <h3>How you can participate:</h3>
+      <ul>
+        <li>
+          <strong>Submit Musician Information:</strong> Musicians, fans,
+          supportive moms- anyone can fill out a few simple forms to add to the
+          directory. Admins approve submissions to ensure directory quality.
+        </li>
+        <li>
+          <strong>Spread the Word:</strong> Everyone can contribute by spreading
+          the word about the Tulsa Musician Directory. The more people who know
+          about our platform, the more vibrant our community becomes.
+        </li>
+        <li>
+          <strong>Feedback and Suggestions:</strong> We value your input. Share
+          your feedback, ideas, and suggestions with us. Your insights help us
+          continuously improve and refine our platform.
+        </li>
+      </ul>
+      <p>
+        Together, we're creating a thriving music directory that not only
+        connects us with our local artists but also celebrates their talents.
+        It's a collaborative effort that fosters a stronger sense of community,
+        empowers our musicians, and enriches the musical tapestry of Tulsa.
+      </p>
+      <a href="https://github.com/nathanhall762/TulsaMusicianDirectory">Visit our Github!</a>
     </>
   );
 };
