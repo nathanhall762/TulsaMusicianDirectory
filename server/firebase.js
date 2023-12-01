1af63f8c75f5e86c6ad263673d17aa13cba68c1a
EthanWakeford <ethanwakeford@gmail.com>
2023-12-01 11:19:59 -0600

    updated variable name

--- server/firebase.js
+++ server/firebase.js
@@ -3,9 +3,9 @@ import { adminDb } from './firebaseAdminConfig.js';
 export async function getMusicians() {
   // open and retrieve collection
   const collection = adminDb.collection('musicians');
-  const snapshot = await collection.get();
+  const collectionSnapshot = await collection.get();
 
   // pull out data from documents into array of muscian info
-  const musicians = snapshot.docs.map((doc) => doc.data());
+  const musicians = collectionSnapshot.docs.map((doc) => doc.data());
   return musicians;
 }
