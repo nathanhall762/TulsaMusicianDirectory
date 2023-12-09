6eb8ce04382bdb00893692b097df6f439b298bb7
EthanWakeford <ethanwakeford@gmail.com>
2023-12-08 22:53:13 -0600

    trying to fix server

--- server/server.js
+++ server/server.js
@@ -32,16 +32,7 @@ if (!isProduction) {
   const compression = (await import('compression')).default;
   const sirv = (await import('sirv')).default;
   app.use(compression());
-  app.use(
-    base,
-    sirv('./dist/client', {
-      setHeaders: (res, path) => {
-        if (path.endsWith('.js')) {
-          res.setHeader('Content-Type', 'application/javascript');
-        }
-      },
-    })
-  );
+  app.use(base, sirv('./dist/client', { extensions: [] }));
   render = (await import('../dist/server/entry.server.js')).render;
 }
 
@@ -64,7 +55,7 @@ app.use('*', async (req, res) => {
 
     const appHtml = await render(req);
     const html = template.replace('<!--app-html-->', appHtml);
-    // res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Type', 'text/html');
 
     // caching????
     res.set('Cache-Control', 'public, max-age=6000, s-maxage=12000');
